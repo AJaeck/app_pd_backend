57afc27f87fc28de4894e3ec6148f24126ca9774
AjaeckAJaeck <ajaeck@gmail.com>
2023-11-01 17:03:55 +0100

    finished basic functionallity

--- app.py
+++ app.py
@@ -5,16 +5,27 @@ import uuid
 from datetime import datetime
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     dob = db.Column(db.Date, nullable=False)
+    # No need to manually define the 'results' relationship here.
+
+
+class Results(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    taps = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship('User', backref=db.backref('results', lazy=True))
 
 @app.route('/')
 def hello_world():
@@ -41,13 +52,54 @@ def get_users():
     users = User.query.all()
     return jsonify([{"id": user.id, "first_name": user.first_name, "last_name": user.last_name, "dob": user.dob.strftime('%d.%m.%Y')} for user in users]), 200
 
-@app.route('/get-user/<user_id>', methods=['GET'])
+@app.route('/get-user-data/<user_id>', methods=['GET'])
 def get_user(user_id):
     user = User.query.get(user_id)
+    print(user)
     if user:
-        return jsonify({"id": user.id, "first_name": user.first_name, "last_name": user.last_name, "dob": user.dob.strftime('%d.%m.%Y')})
+        user_data = {
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "dob": user.dob.strftime('%d.%m.%Y'),
+            "results": [{"date": result.date.strftime('%Y-%m-%d'), "taps": result.taps} for result in user.results]
+        }
+        return jsonify(user_data), 200
     else:
         return jsonify({"message": "User not found"}), 404
+
+@app.route('/delete-user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    # Delete associated results
+    Results.query.filter_by(user_id=user_id).delete()
+
+    # Delete the user
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "User and associated data deleted successfully!"}), 200
+
+
+@app.route('/save-tapping-result/<user_id>', methods=['POST'])
+def save_tapping_result(user_id):
+    data = request.json
+
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    tapping_result = Results(user_id=user_id, date=datetime.now(), taps=data['taps'])
+    db.session.add(tapping_result)
+    db.session.commit()
+    return jsonify({"message": "Tapping result saved successfully!"}), 201
+
+@app.route('/get-tapping-results/<user_id>', methods=['GET'])
+def get_tapping_results(user_id):
+    results = Results.query.filter_by(user_id=user_id).all()
+    return jsonify([{"date": result.date.strftime('%Y-%m-%d'), "taps": result.taps} for result in results]), 200
 
 def init_db():
     with app.app_context():
